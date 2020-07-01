c56e669a87a8feeaca1b5e5aa2e7058015db6273
hat_lover <jobin25051999@gmail.com>
2020-07-01 09:10:00 +0530

    final updates july 1

--- Calculator.py
+++ Calculator.py
@@ -1,16 +1,30 @@
 from tkinter import*
 
-expression = "" 
+# initial set
+
 calc_app=Tk()
 calc_app.resizable(0,0)
 calc_app.title("Python Calculator")
-calc_app.geometry("300x400")
+calc_app.geometry("300x600")
+calc_app.configure(bg="black")
+
+
+#Grid configure
+
+calc_app.grid_rowconfigure(0, weight=1)
 calc_app.grid_rowconfigure(1, weight=1)
-calc_app.columnconfigure(1, weight=1) 
-#calc_frame=Frame(calc_app,bg="gray",width=300,height=400)
-#calc_frame.grid(row=0,column=0,sticky="NW")
-#calc_frame.grid_propagate(0)
-#calc_frame.update()
+calc_app.grid_rowconfigure(2, weight=1)
+calc_app.grid_rowconfigure(3, weight=1)
+calc_app.grid_rowconfigure(4, weight=1)
+calc_app.grid_rowconfigure(5, weight=1)
+calc_app.grid_rowconfigure(6, weight=1)
+calc_app.grid_columnconfigure(0, weight=1)
+calc_app.grid_columnconfigure(1, weight=1)
+calc_app.grid_columnconfigure(2, weight=1)
+
+# Calculations
+
+expression = "" 
 equation = StringVar()
 
 def press(num):
@@ -18,16 +32,17 @@ def press(num):
     expression = expression + str(num) 
     equation.set(expression)
 def equalpress():
+   global expression
    total = str(eval(expression)) 
    equation.set(total)
+   expression=total
 def clear():
     global expression
     expression=""
     equation.set(expression)
     
     
-#home_label=Label(calc_app,text="PYTHON CALCULATOR")
-#text_box_main=Text(calc_app,height=4,width=30)
+# widgets set
 
 text_box_main = Entry(calc_app, textvariable=equation) 
 button_1=Button(calc_app,width=10,height=3,text="1",command=lambda:press(1))
@@ -48,28 +63,35 @@ button_div=Button(calc_app,width=10,height=3,text="/",command=lambda:press("/"))
 button_dec=Button(calc_app,width=10,height=3,text=".",command=lambda:press("."))
 button_equal=Button(calc_app,width=10,height=3,text="=",command=lambda:equalpress())
 button_clr=Button(calc_app,width=10,height=3,text="CLR",command=lambda:clear())
+button_exit=Button(calc_app,width=10,height=3,text="Exit",command=lambda:exit())
 
 
-#home_label.grid(row=0,column=0,sticky = W, pady = 2)
-#text_box_main.grid(row=1,column=0)
-text_box_main.grid(columnspan=3,padx=2, ipadx=300) 
-button_1.grid(row=2,column=0)
-button_2.grid(row=2,column=1)
-button_3.grid(row=2,column=2)
-button_4.grid(row=3,column=0)
-button_5.grid(row=3,column=1)
-button_6.grid(row=3,column=2)
-button_7.grid(row=4,column=0)
-button_8.grid(row=4,column=1)
-button_9.grid(row=4,column=2)
+# Grid alignment
 
-button_0.grid(row=5,column=0)
-button_plus.grid(row=5,column=1)
-button_minus.grid(row=5,column=2)
-button_mul.grid(row=6,column=0)
-button_div.grid(row=6,column=1)
-button_dec.grid(row=6,column=2)
-button_equal.grid(row=7,column=0)
-button_clr.grid(row=7,column=1)
+text_box_main.grid(columnspan=3,padx=2,pady=2, ipadx=300,sticky="nsew")
+
+button_1.grid(row=2,column=0,sticky="nsew")
+button_2.grid(row=2,column=1,sticky="nsew")
+button_plus.grid(row=2,column=2,sticky="nsew")
+
+button_3.grid(row=3,column=0,sticky="nsew")
+button_4.grid(row=3,column=1,sticky="nsew")
+button_minus.grid(row=3,column=2,sticky="nsew")
+
+button_5.grid(row=4,column=0,sticky="nsew")
+button_6.grid(row=4,column=1,sticky="nsew")
+button_mul.grid(row=4,column=2,sticky="nsew")
+
+button_7.grid(row=5,column=0,sticky="nsew")
+button_8.grid(row=5,column=1,sticky="nsew")
+button_div.grid(row=5,column=2,sticky="nsew")
+
+button_9.grid(row=6,column=0,sticky="nsew")
+button_0.grid(row=6,column=1,sticky="nsew")
+button_equal.grid(row=6,column=2,sticky="nsew")
+
+button_dec.grid(row=7,column=0,sticky="nsew")
+button_clr.grid(row=7,column=1,sticky="nsew")
+button_exit.grid(row=7,column=2,sticky="nsew")
 
 calc_app.mainloop()
